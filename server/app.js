4b7a2d518bd89258d90c57b1bfab134d01e2aea9
Gary Teh <garyjob@Garys-MacBook-Air.local>
2012-08-11 15:17:50 -0700

    the latest web server

--- server/app.js
+++ server/app.js
@@ -4,5 +4,36 @@ var pcap_session = pcap.createSession("", "");
 var packetNumber = 0;
 pcap_session.on('packet', function (raw_packet) {
     // do some stuff with a raw packet
-  console.log(raw_packet);
+  //console.log(raw_packet);
+  var packet = pcap.decode.packet(raw_packet);
+  //console.log(packet);
 });
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.methodOverride());
+  app.use(express.errorHandler());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+
+
+app.get('/', function(req, res){
+	console.log("Line 30");
+	res.render('index',{});
+});
+
+app.error(function(err, req, res, next) {
+  res.render('error', {error: err});
+});
+
+app.listen(7777);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
